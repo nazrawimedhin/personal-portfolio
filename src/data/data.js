39713b64bc645cacf4cc742cd680810ac48cd3c9
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2023-11-19 17:30:12 +0300

    some new data's added

--- src/data/data.js
+++ src/data/data.js
@@ -1,45 +1,48 @@
-import projImg1 from "../assets/img/h1.png";
-import projImg2 from "../assets/img/r1.png";
-import projImg3 from "../assets/img/d1.png";
+import webImg1 from "../assets/img/h1.png";
+import webImg2 from "../assets/img/r1.png";
+import webImg3 from "../assets/img/d1.png";
+import desImg1 from "../assets/img/m1.png";
+import desImg2 from "../assets/img/f1.png";
+import desImg3 from "../assets/img/c1.jpg";
 
 export const webProjects = [
   {
     title: "Card and Payment Website",
     description: "Design & Development",
-    img: projImg1,
+    img: webImg1,
     url: "https://hoobank-cards.netlify.app/",
   },
   {
     title: "Real Estate Website",
     description: "Design & Development",
-    img: projImg2,
+    img: webImg2,
     url: "https://real-estate-website-v1.netlify.app/",
   },
   {
     title: "Digital Marketing Website",
     description: "Design & Development",
-    img: projImg3,
+    img: webImg3,
     url: "https://nazrawimedhin.github.io/enkopazion-digital-marketing/",
   },
 ];
 
 export const UIUXProjects = [
   {
-    title: "Card and Payment Website",
-    description: "Design & Development",
-    img: projImg1,
-    url: "https://hoobank-cards.netlify.app/",
+    title: "Metaverse UI",
+    description: "Design and Presentation",
+    img: desImg1,
+    url: "https://www.canva.com/design/DAF0mOJ8Oiw/E1ZuePAP2N5UdD38Oq6Bog/edit?utm_content=DAF0mOJ8Oiw&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
   },
   {
-    title: "Real Estate Website",
-    description: "Design & Development",
-    img: projImg2,
-    url: "https://real-estate-website-v1.netlify.app/",
+    title: "Food Delivery App",
+    description: "Design & Presentation",
+    img: desImg2,
+    url: "https://www.canva.com/design/DAF0mnouB-s/1dTpEbd3TuvlWERMK0xIeQ/edit?utm_content=DAF0mnouB-s&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
   },
   {
-    title: "Digital Marketing Website",
-    description: "Design & Development",
-    img: projImg3,
-    url: "https://nazrawimedhin.github.io/enkopazion-digital-marketing/",
+    title: "Card and Payment",
+    description: "Design & Presentation",
+    img: desImg3,
+    url: "https://www.canva.com/design/DAF0mrQ3nNc/Hkt_n-SJj6Qdd_t1m_yWZw/edit?utm_content=DAF0mrQ3nNc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
   },
 ];
